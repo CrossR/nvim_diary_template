5c2099381c225b330d10aa840b1853316f88e7a0
Ryan C <sc13rjc@leeds.ac.uk>
2018-05-25 19:25:02 +0100

    Simple wrapper func to make a full markdown file.

---
+++ gcal_example/make_markdown_file.py
@@ -0,0 +1,18 @@
+from make_schedule import make_schedule
+
+# TODO: Plug this in as a config option
+HEADINGS = ['Notes', 'Issues', 'ToDo']
+
+def produce_daily_markdown(headings):
+    full_markdown_file = []
+
+    for heading in headings:
+        full_markdown_file.append(f"# {heading}")
+        full_markdown_file.append("\n")
+    
+    full_markdown_file.extend(make_schedule())
+
+    return full_markdown_file
+
+if __name__ == "__main__":
+    produce_daily_markdown(HEADINGS)
