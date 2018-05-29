ae9e33a834a50dd438215f54ef7b659326f3866f
CrossR <sc13rjc@leeds.ac.uk>
2018-05-29 20:05:17 +0100

    Remove newlines from Markdown generation.

--- rplugin/python3/nvim_notes/utils/make_markdown_file.py
+++ rplugin/python3/nvim_notes/utils/make_markdown_file.py
@@ -1,11 +1,16 @@
 from .make_schedule import make_schedule
 
 def produce_daily_markdown(options):
+    """produce_daily_markdown
+
+    Produce the actual markdown that is shown on the page.
+    """
+
     full_markdown_file = []
 
     for heading in options.headings:
         full_markdown_file.append(f"# {heading}")
-        full_markdown_file.append("\n")
+        full_markdown_file.append("")
 
     full_markdown_file.extend(make_schedule(options))
 
