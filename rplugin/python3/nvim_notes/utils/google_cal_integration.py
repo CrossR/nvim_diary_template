d9ab4a2c55d7f1ce7d332ac9e5128357778c58f3
CrossR <sc13rjc@leeds.ac.uk>
2018-05-30 13:23:25 +0100

    Attempt to fix cache logic.

--- rplugin/python3/nvim_notes/utils/google_cal_integration.py
+++ rplugin/python3/nvim_notes/utils/google_cal_integration.py
@@ -1,13 +1,31 @@
+import glob
+import json
+import re
+import time as t
 from datetime import date, datetime, time
-from os import path
+from os import makedirs, path
 
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import client, file, tools
+from functools import wraps
 
-from .function_wrappers import SimpleCache, check_service
-
+CACHE_EPOCH_REGEX = '([0-9])+'
 CALENDAR_CACHE_DURATION = 31
+
+
+def check_service(function):
+    """check_service
+
+    A decorator to check the Google cal service exists
+    """
+
+    @wraps(function)
+    def wrapper(self):
+        if self.service is None:
+            return
+        function(self)
+    return wrapper
 
 
 class SimpleNvimGoogleCal():
@@ -18,7 +36,11 @@ class SimpleNvimGoogleCal():
 
         self.service = self.setup_google_calendar_api()
 
-        self.all_calendars = self.get_all_calendars()
+        self.all_calendars = self.check_cache(
+            "calendars",
+            31,
+            self.get_all_calendars()
+        )
 
         self.filter_list = options.calendar_filter_list
         self.filtered_calendars = self.filter_calendars()
@@ -53,7 +75,6 @@ class SimpleNvimGoogleCal():
         return [cal for cal in self.all_calendars if cal not in self.filter_list]
 
     @check_service
-    @SimpleCache('calendar', CALENDAR_CACHE_DURATION)
     def get_all_calendars(self):
         """get_all_calendars
 
@@ -134,6 +155,45 @@ class SimpleNvimGoogleCal():
             filtered_events.append(event_dict)
 
         return filtered_events
+
+    def check_cache(self, data_name, data_age, fallback_function):
+        """check_cache
+
+        A function decorator to check for valid cache files.
+        If one is found, then it can be used, but otherwise the original function
+        is called to generate the data and cache it.
+        """
+
+        pattern = f"{self.config_path}/cache/" + \
+            f"nvim_notes_{data_name}_cache_*.json"
+
+        try:
+            cache_file_name = glob.glob(pattern)[0]
+
+            epoch = re.search(CACHE_EPOCH_REGEX, cache_file_name)[0]
+
+            cache_file_creation_date = datetime.fromtimestamp(int(epoch))
+            today = datetime.today()
+            difference = today - cache_file_creation_date
+
+            if difference.days <= data_age:
+                with open(cache_file_name) as cache_file:
+                    data = json.load(cache_file)
+        except (IndexError, FileNotFoundError):
+            data = fallback_function(self)
+            self.set_cache(data, data_name)
+
+        return data
+
+    def set_cache(self, data, data_name):
+
+        cache_file_name = f"{self.config_path}/cache/" + \
+            f"nvim_notes_{data_name}_cache_{int(t.time())}.json"
+
+        makedirs(path.dirname(cache_file_name), exist_ok=True)
+
+        with open(cache_file_name, 'w') as cache_file:
+            json.dump(data, cache_file)
 
     def compare_events(self, markdown_events):
         events_today = self.get_events_for_timeframe()
