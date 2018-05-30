f8188ab4f8b3792878ecbb3736ff0cd8a5aa4482
CrossR <sc13rjc@leeds.ac.uk>
2018-05-30 16:50:28 +0100

    Start on an event line parser.

--- rplugin/python3/nvim_notes/utils/make_markdown_file.py
+++ rplugin/python3/nvim_notes/utils/make_markdown_file.py
@@ -15,3 +15,32 @@ def produce_daily_markdown(nvim, options):
     full_markdown_file.extend(make_schedule(nvim, options))
 
     return full_markdown_file
+
+def parse_buffer_events(events):
+    formatted_events = []
+
+    for event in events:
+        if event == '':
+            continue
+
+        format_string = f"    - {start_time} - {end_time}: {event_name}"
+
+
+def parse_markdown_file_for_events(nvim):
+    buffer_number = nvim.current.buffer.number
+    current_buffer_contents = nvim.api.buf_get_lines(
+        buffer_number,
+        0,
+        -1,
+        True
+    )
+
+    # Do the search in reverse since we know the schdule comes last
+    for line_index, line in enumerate(reversed(current_buffer_contents)):
+        if line == '# Schedule':
+            buffer_events_index = line_index
+
+    buffer_events_index = len(current_buffer_contents) - buffer_events_index
+    events = current_buffer_contents[buffer_events_index:]
+    formatted_events = parse_buffer_events(events)
+
