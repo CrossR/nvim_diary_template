9bf5272876dc5ce72b0552e7350d34f3f7756b99
CrossR <sc13rjc@leeds.ac.uk>
2018-05-31 22:51:39 +0100

    Refactor PluginOptions out.

--- rplugin/python3/nvim_notes/plugin.py
+++ rplugin/python3/nvim_notes/plugin.py
@@ -1,6 +1,4 @@
-import os
 from functools import wraps
-from pathlib import Path
 
 import neovim
 
@@ -8,6 +6,7 @@ from nvim_notes.utils.google_cal_integration import SimpleNvimGoogleCal
 from nvim_notes.utils.keybind_actions import strikeout_line
 from nvim_notes.utils.make_markdown_file import (make_markdown_file,
                                                  parse_markdown_file_for_events)
+from nvim_notes.utils.plugin_options import PluginOptions
 
 FILE_TYPE = '*.md'
 
@@ -86,26 +85,3 @@ class NotesPlugin(object):
             True,
             strikeout_line(current_line[0])
         )
-
-
-class PluginOptions:
-
-    _defaults = {
-        'active': True,
-        'config_path': os.getcwd(),
-        'notes_path': os.path.join(str(Path.home()), "nvim_notes"),
-        'open_method': 'tabnew',
-        'headings': ['Notes', 'Issues', 'ToDo'],
-        'use_google_calendar': True,
-        'calendar_filter_list': [],
-        'add_to_google_cal': False,
-        'google_cal_name': 'primary',
-        'timezone': 'Europe/London'
-    }
-
-    def __init__(self, nvim):
-        for key, default_value in PluginOptions._defaults.items():
-            value = nvim.vars.get(f"nvim_notes#{key}", default_value)
-            nvim.vars[f"nvim_notes#{key}"] = value
-
-            setattr(self, key, value)
---
+++ rplugin/python3/nvim_notes/utils/plugin_options.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+class PluginOptions:
+
+    _defaults = {
+        'active': True,
+        'config_path': os.getcwd(),
+        'notes_path': os.path.join(str(Path.home()), "nvim_notes"),
+        'open_method': 'tabnew',
+        'headings': ['Notes', 'Issues', 'ToDo'],
+        'use_google_calendar': True,
+        'calendar_filter_list': [],
+        'add_to_google_cal': False,
+        'google_cal_name': 'primary',
+        'timezone': 'Europe/London'
+    }
+
+    def __init__(self, nvim):
+        for key, default_value in PluginOptions._defaults.items():
+            value = nvim.vars.get(f"nvim_notes#{key}", default_value)
+            nvim.vars[f"nvim_notes#{key}"] = value
+
+            setattr(self, key, value)
