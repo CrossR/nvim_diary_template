c986126fb531dc0ad421939d64188c24343e31b6
CrossR <sc13rjc@leeds.ac.uk>
2018-05-31 15:24:41 +0100

    Keep the start of the line.

--- rplugin/python3/nvim_notes/utils/keybind_actions.py
+++ rplugin/python3/nvim_notes/utils/keybind_actions.py
@@ -1,4 +1,6 @@
 def strikeout_line(line):
     line_content = line.strip().split()[1:]
-    return [f"~~{' '.join(line_content)}~~"]
+    first_non_space = len(line) - len(line.strip())
+
+    return [f"{line[:first_non_space]}~~{' '.join(line_content)}~~"]
 
