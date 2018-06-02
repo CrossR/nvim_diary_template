2501330e750d081d60ae6b73ae008d05d6ce1704
Ryan C <sc13rjc@leeds.ac.uk>
2018-06-02 18:18:56 +0100

    Fix typo.

--- rplugin/python3/nvim_notes/utils/SimpleNvimGoogleCal.py
+++ rplugin/python3/nvim_notes/utils/SimpleNvimGoogleCal.py
@@ -34,7 +34,7 @@ class SimpleNvimGoogleCal():
         self.filter_list = options.calendar_filter_list
         self.filtered_calendars = self.filter_calendars()
 
-        self.set_cache(self.filter_calendars, 'filtered')
+        self.set_cache(self.filtered_calendars, 'filtered')
 
         self._events = self.check_cache(
             'events',
