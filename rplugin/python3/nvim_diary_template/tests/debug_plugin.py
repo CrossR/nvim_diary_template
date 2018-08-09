be52667e8ca3d8599e6072f5cd1ce2f0cd83a020
Ryan C <sc13rjc@leeds.ac.uk>
2018-08-09 19:27:59 +0100

    Run linter.

--- rplugin/python3/nvim_diary_template/tests/debug_plugin.py
+++ rplugin/python3/nvim_diary_template/tests/debug_plugin.py
@@ -4,7 +4,10 @@ import os
 
 from nvim_diary_template.plugin import DiaryTemplatePlugin
 
-pipes = os.listdir('\\\\.\\pipe')
-current_pipe = [pipe for pipe in pipes if pipe.startswith('nvim')][0]
-nvim = neovim.attach('socket', path=f"\\\\.\\pipe\\{current_pipe}")
-DiaryTemplatePlugin(nvim).make_diary()
+# A basic script to start up a neovim instance and start the plugin running.
+# This is intended to be used with a debugger, to move through the plugin.
+PIPES = os.listdir("\\\\.\\pipe")
+CURRENT_PIPE = [pipe for pipe in PIPES if pipe.startswith("nvim")][0]
+NVIM = neovim.attach("socket", path=f"\\\\.\\pipe\\{CURRENT_PIPE}")
+DiaryTemplatePlugin(NVIM).make_diary()
+
