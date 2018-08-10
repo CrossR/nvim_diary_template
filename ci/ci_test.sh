916e4f95b8430ebbe6b61dc1002db16835f27155
Ryan C <sc13rjc@leeds.ac.uk>
2018-08-10 22:04:28 +0100

    Update CI script.
    For now, lets allow these two cases, but run two CI scripts so they can be kept an eye on.

--- ci/ci_test.sh
+++ ci/ci_test.sh
@@ -8,20 +8,30 @@ echo "Checking code syntax with Black..."
 black rplugin/python3/deoplete --check
 
 if [ $? -ne 0 ]; then
-    echo "Deoplete failed black check."
+    echo "Deoplete failed black check..."
     exit ${FAIL}
 fi
 
 black rplugin/python3/nvim_diary_template --check
 
 if [ $? -ne 0 ]; then
-    echo "Main code failed black check."
+    echo "Main code failed black check..."
     exit ${FAIL}
 fi
 
 echo "Running mypy on code base..."
 echo "TODO: Once fully updated, check the error code here."
-mypy rplugin/python3/nvim_diary_template --strict
+if [ "${FULL_TYPING}" -eq 1 ]; then
+    mypy rplugin/python3/nvim_diary_template --strict
+    false # Should return 0 every time, meaning the CI doesn't stop
+else
+    mypy rplugin/python3/nvim_diary_template --strict --allow-untyped-calls --allow-untyped-decorators
+fi
+
+if [ $? -ne 0 ]; then
+    echo "Failed typing checks..."
+    exit ${FAIL}
+fi
 
 echo "Script finished!"
 exit ${SUCCESS}
