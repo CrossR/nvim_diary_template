0a03860a68b528f0e287470d6e799a2509e0ff3c
Ryan C <sc13rjc@leeds.ac.uk>
2018-08-10 22:41:23 +0100

    Update script for change to Pipenv.

--- ci/ci_test.sh
+++ ci/ci_test.sh
@@ -2,17 +2,17 @@ SUCCESS=0
 FAIL=1
 
 echo "Linting code..."
-pylint rplugin/python3/nvim_diary_template 
+pipenv Running pylint rplugin/python3/nvim_diary_template 
 
 echo "Checking code syntax with Black..."
-black rplugin/python3/deoplete --check
+pipenv run black rplugin/python3/deoplete --check
 
 if [ $? -ne 0 ]; then
     echo "Deoplete failed black check..."
     exit ${FAIL}
 fi
 
-black rplugin/python3/nvim_diary_template --check
+pipenv run black rplugin/python3/nvim_diary_template --check
 
 if [ $? -ne 0 ]; then
     echo "Main code failed black check..."
@@ -21,10 +21,10 @@ fi
 
 echo "Running mypy on code base..."
 if [ "${FULL_TYPING:-0}" -eq 1 ]; then
-    mypy rplugin/python3/nvim_diary_template --strict
+    pipenv run mypy rplugin/python3/nvim_diary_template --strict
     false # Should return 0 every time, meaning the CI doesn't stop
 else
-    mypy rplugin/python3/nvim_diary_template --strict --allow-untyped-calls --allow-untyped-decorators
+    pipenv run mypy rplugin/python3/nvim_diary_template --strict --allow-untyped-calls --allow-untyped-decorators
 fi
 
 if [ $? -ne 0 ]; then
