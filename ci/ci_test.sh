5caae8ba33d372d41d1bd35b15829df8d5fbf577
Ryan C <sc13rjc@leeds.ac.uk>
2018-08-11 01:14:01 +0100

    More CI updates.

--- ci/ci_test.sh
+++ ci/ci_test.sh
@@ -1,29 +1,36 @@
 SUCCESS=0
 FAIL=1
 
-echo "Linting code..."
-pipenv run pylint rplugin/python3/nvim_diary_template 
+echo "Starting testing script..."
 
-echo "Checking code syntax with Black..."
-pipenv run black rplugin/python3/deoplete --check
+if [ "${LINT_CODE:-0}" -eq 1 ]; then
+    echo "Linting code..."
+    pipenv run pylint rplugin/python3/nvim_diary_template 
 
-if [ $? -ne 0 ]; then
-    echo "Deoplete failed black check..."
-    exit ${FAIL}
+    echo "Checking code syntax with Black..."
+    pipenv run black rplugin/python3/deoplete --check
+
+    if [ $? -ne 0 ]; then
+        echo "Deoplete failed black check..."
+        exit ${FAIL}
+    fi
+
+    pipenv run black rplugin/python3/nvim_diary_template --check
+
+    if [ $? -ne 0 ]; then
+        echo "Main code failed black check..."
+        exit ${FAIL}
+    fi
 fi
 
-pipenv run black rplugin/python3/nvim_diary_template --check
-
-if [ $? -ne 0 ]; then
-    echo "Main code failed black check..."
-    exit ${FAIL}
-fi
-
-echo "Running mypy on code base..."
 if [ "${FULL_TYPING:-0}" -eq 1 ]; then
+    echo "Running full mypy on code base..."
     pipenv run mypy rplugin/python3/nvim_diary_template --strict --ignore-missing-imports
     false # Should return 0 every time, meaning the CI doesn't stop
-else
+fi
+
+if [ "${BASIC_TYPING-0}" -eq 1 ]; then
+    echo "Running basic mypy on code base..."
     pipenv run mypy --config-file mypy.ini rplugin/python3/nvim_diary_template
 fi
 
