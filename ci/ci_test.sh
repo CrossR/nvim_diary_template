d0256301c47af5a20b83c0eea95e4c7508b704e5
Ryan C <sc13rjc@leeds.ac.uk>
2018-08-11 16:22:39 +0100

    Add a logging step to the CI.

--- ci/ci_test.sh
+++ ci/ci_test.sh
@@ -3,7 +3,13 @@ FAIL=1
 
 echo "Starting testing script..."
 
+pipenv run python --version
+
 if [ "${LINT_CODE:-0}" -eq 1 ]; then
+
+    pipenv run pylint --version
+    pipenv run black --version
+
     echo "Linting code..."
     pipenv run pylint rplugin/python3/nvim_diary_template 
 
@@ -24,22 +30,32 @@ if [ "${LINT_CODE:-0}" -eq 1 ]; then
 fi
 
 if [ "${FULL_TYPING:-0}" -eq 1 ]; then
+
+    pipenv run mypy --version
+
     echo "Running full mypy on code base..."
     pipenv run mypy rplugin/python3/nvim_diary_template --strict
 
     if [ $? -ne 0 ]; then
         echo "Full typing check failed..."
         exit ${FAIL}
+    else
+        echo "Full typing check passed!"
     fi
 fi
 
 if [ "${BASIC_TYPING-0}" -eq 1 ]; then
+
+    pipenv run mypy --version
+
     echo "Running basic mypy on code base..."
     pipenv run mypy --config-file mypy.ini rplugin/python3/nvim_diary_template
 
     if [ $? -ne 0 ]; then
         echo "Basic typing check failed..."
         exit ${FAIL}
+    else
+        echo "Basic typing check passed!"
     fi
 fi
 
