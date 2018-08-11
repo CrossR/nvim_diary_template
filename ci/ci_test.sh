68c4f6960644b9853028fe1e4560570ab354ed49
Ryan C <sc13rjc@leeds.ac.uk>
2018-08-11 14:46:58 +0100

    Fail on full typing tests.
    Its added as an allowed failure for now.

--- ci/ci_test.sh
+++ ci/ci_test.sh
@@ -25,13 +25,22 @@ fi
 
 if [ "${FULL_TYPING:-0}" -eq 1 ]; then
     echo "Running full mypy on code base..."
-    pipenv run mypy rplugin/python3/nvim_diary_template --strict --ignore-missing-imports
-    false # Should return 0 every time, meaning the CI doesn't stop
+    pipenv run mypy rplugin/python3/nvim_diary_template --strict
+
+    if [ $? -ne 0 ]; then
+        echo "Full typing check failed..."
+        exit ${FAIL}
+    fi
 fi
 
 if [ "${BASIC_TYPING-0}" -eq 1 ]; then
     echo "Running basic mypy on code base..."
     pipenv run mypy --config-file mypy.ini rplugin/python3/nvim_diary_template
+
+    if [ $? -ne 0 ]; then
+        echo "Basic typing check failed..."
+        exit ${FAIL}
+    fi
 fi
 
 echo "Script finished!"
