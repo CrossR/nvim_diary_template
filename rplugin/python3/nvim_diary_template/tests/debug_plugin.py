6e7030030bb92bebcee6a358bee8896bed0dcfe7
Ryan C <sc13rjc@leeds.ac.uk>
2018-08-13 13:59:11 +0100

    Add some missing types.
    Also refactor the option init to a function to aid debugging.

--- rplugin/python3/nvim_diary_template/tests/debug_plugin.py
+++ rplugin/python3/nvim_diary_template/tests/debug_plugin.py
@@ -1,14 +1,19 @@
 # pylint: disable=all
+# A basic script to start up a neovim instance and start the plugin running.
+# This is intended to be used with a debugger, to move through the plugin.
 import neovim
 import os
 from typing import List
 
 from nvim_diary_template.plugin import DiaryTemplatePlugin
 
-# A basic script to start up a neovim instance and start the plugin running.
-# This is intended to be used with a debugger, to move through the plugin.
+# Get the current nvim instance. If there are multiple instances, it may be
+# more reliable to setup a dedicated address, see:
+# http://pynvim.readthedocs.io/en/latest/development.html#usage-through-the-python-repl
 PIPES: List[str] = os.listdir("\\\\.\\pipe")
 CURRENT_PIPE: str = [pipe for pipe in PIPES if pipe.startswith("nvim")][0]
 NVIM: neovim.Nvim = neovim.attach("socket", path=f"\\\\.\\pipe\\{CURRENT_PIPE}")
 
+# Initialise the options, then run the required function.
+DiaryTemplatePlugin(NVIM).check_options()
 DiaryTemplatePlugin(NVIM).grab_from_calendar()
