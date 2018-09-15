87042149797986df36ee06f59e45c6ea26295798
Ryan C <r.cross@lancaster.ac.uk>
2018-09-15 22:29:33 +0100

    Finish make markdown tests.

--- rplugin/python3/nvim_diary_template/tests/test_make_markdown_file.py
+++ rplugin/python3/nvim_diary_template/tests/test_make_markdown_file.py
@@ -1,11 +1,11 @@
 
 import unittest
-from typing import Any
+from typing import Any, Dict, List
 
 from dateutil import parser
 
 from ..classes.calendar_event_class import CalendarEvent
-from ..classes.github_issue_class import GitHubIssue
+from ..classes.github_issue_class import GitHubIssue, GitHubIssueComment
 from ..utils.constants import ISO_FORMAT
 from ..utils.make_markdown_file import generate_markdown_metadata, make_todays_diary
 from .mocks.gcal import MockGCalService
@@ -51,11 +51,100 @@ class make_markdown_fileTest(unittest.TestCase):
                 ),
             ]
         )
-        # Check doesn't save over modified.
+
+        # Check events are added.
         nvim = MockNvim()
         make_todays_diary(nvim, options, gcal, github)
         assert len(nvim.current.buffer.lines) == 15
-        assert nvim.commands == [":w"]
+
+        # But not when disabled.
+        options.use_google_calendar = False
+        nvim = MockNvim()
+        make_todays_diary(nvim, options, gcal, github)
+        assert len(nvim.current.buffer.lines) == 13
+        options.use_google_calendar = True
+
+        github.issues.append(
+            GitHubIssue(
+                number=1,
+                title="Test Issue 1",
+                complete=False,
+                labels=[],
+                metadata=[],
+                all_comments=[
+                    GitHubIssueComment(
+                        number=0,
+                        body=["Test comment body."],
+                        tags=[],
+                        updated_at=parser.parse("2018-01-01 12:00").strftime(
+                            ISO_FORMAT
+                        ),
+                    )
+                ],
+            )
+        )
+
+        # Check issues are added.
+        nvim = MockNvim()
+        make_todays_diary(nvim, options, gcal, github)
+        assert len(nvim.current.buffer.lines) == 22
+
+        # But not when disabled.
+        options.use_github_repo = False
+        nvim = MockNvim()
+        make_todays_diary(nvim, options, gcal, github)
+        assert len(nvim.current.buffer.lines) == 15
+        options.use_github_repo = True
+
+        nvim = MockNvim()
+        make_todays_diary(nvim, options, gcal, github)
+        final_markdown: List[str] = [
+            "",
+            "## Notes",
+            "",
+            "## Issues",
+            "",
+            "### [ ] Issue {1}:",
+            "",
+            "#### Title: Test Issue 1",
+            "",
+            "#### Comment {0} - 2018-01-01T12:00:00.000000:",
+            "Test comment body.",
+            "",
+            "## Schedule",
+            "",
+            "- 10:00 - 11:00: Event 1",
+            "- 19:00 - 22:00: Event 2",
+            "",
+        ]
+
+        assert final_markdown == nvim.current.buffer.lines[5:]
 
     def test_generate_markdown_metadata(self) -> None:
-        raise NotImplementedError()  # TODO: test generate_markdown_metadata
+
+        diary_metadata: Dict[str, str] = {"Date": "2018-01-01"}
+        result: List[str] = generate_markdown_metadata(diary_metadata)
+
+        example: List[str] = [
+            "<!---",
+            "    Date: 2018-01-01",
+            "    Tags:",
+            "--->",
+            "# Diary for 2018-01-01",
+            "",
+        ]
+        assert result == example
+
+        diary_metadata = {"Date": "2018-01-01", "Title": "Python Unit Tests"}
+        result = generate_markdown_metadata(diary_metadata)
+
+        example = [
+            "<!---",
+            "    Date: 2018-01-01",
+            "    Title: Python Unit Tests",
+            "    Tags:",
+            "--->",
+            "# Diary for 2018-01-01",
+            "",
+        ]
+        assert result == example
