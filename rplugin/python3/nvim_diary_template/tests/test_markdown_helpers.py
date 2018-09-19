ae9d8cdbb642dc68ddb3e3d75a7014dcfa655408
Ryan C <r.cross@lancaster.ac.uk>
2018-09-19 21:13:02 +0100

    Update tests.

--- rplugin/python3/nvim_diary_template/tests/test_markdown_helpers.py
+++ rplugin/python3/nvim_diary_template/tests/test_markdown_helpers.py
@@ -1,26 +1,67 @@
 
 import unittest
 
+from typing import List
+
+from .mocks.nvim import MockNvim
+from ..helpers.markdown_helpers import sort_markdown_events
+
 
 class markdown_helpersTest(unittest.TestCase):
     """
     Tests for functions in the markdown_helpers module.
     """
 
-    def setUp(self) -> None:
-        pass  # TODO
-
-    def test_get_start_of_line(self) -> None:
-        raise NotImplementedError()  # TODO: test get_start_of_line
-
     def test_sort_markdown_events(self) -> None:
-        raise NotImplementedError()  # TODO: test sort_markdown_events
 
-    def test_split_line(self) -> None:
-        raise NotImplementedError()  # TODO: test split_line
+        nvim: MockNvim = MockNvim()
+        nvim.current.buffer.lines = [
+            "<!---",
+            "    Date: 2018-01-01",
+            "    Tags:",
+            "--->",
+            "# Diary for 2018-01-01",
+            "",
+            "## Notes",
+            "",
+            "## Issues",
+            "",
+            "## Schedule",
+            "",
+            "- 10:00 - 11:00: Event 2",
+            "- 14:00 - 16:00: Event 4",
+            "- 14:00 - 15:00: Event 3",
+            "- 10:00 - 11:00: Event 1",
+            "",
+        ]
 
-    def test_get_line_padding(self) -> None:
-        raise NotImplementedError()  # TODO: test get_line_padding
+        final_buffer: List[str] = [
+            "<!---",
+            "    Date: 2018-01-01",
+            "    Tags:",
+            "--->",
+            "# Diary for 2018-01-01",
+            "",
+            "## Notes",
+            "",
+            "## Issues",
+            "",
+            "## Schedule",
+            "",
+            "- 10:00 - 11:00: Event 1",
+            "- 10:00 - 11:00: Event 2",
+            "- 14:00 - 15:00: Event 3",
+            "- 14:00 - 16:00: Event 4",
+            "",
+        ]
 
-    def test_apply_padding(self) -> None:
-        raise NotImplementedError()  # TODO: test apply_padding
+        sort_markdown_events(nvim)
+        assert nvim.current.buffer.lines == final_buffer
+
+        # Calling when sorted should mean no change.
+        nvim.api.get_count = 0
+        nvim.api.set_count = 0
+        sort_markdown_events(nvim)
+        assert nvim.current.buffer.lines == final_buffer
+        assert nvim.api.get_count == 1
+        assert nvim.api.set_count == 0
