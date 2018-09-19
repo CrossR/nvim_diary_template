fa6944b4c7f47f58f3e85abe56d7a4e4b3050d7d
Ryan C <r.cross@lancaster.ac.uk>
2018-09-19 21:00:15 +0100

    Remove unneeded functions.

--- rplugin/python3/nvim_diary_template/helpers/markdown_helpers.py
+++ rplugin/python3/nvim_diary_template/helpers/markdown_helpers.py
@@ -14,18 +14,6 @@ from ..utils.make_schedule import set_schedule_from_events_list
 from ..utils.parse_markdown import parse_markdown_file_for_events
 
 
-def get_start_of_line(line: str) -> str:
-    """get_start_of_line
-
-    Return the start of a given line.
-    """
-
-    first_non_space: int = len(line) - len(line.strip())
-    start_of_line: str = line[: first_non_space + 1]
-
-    return start_of_line
-
-
 def sort_markdown_events(nvim: Nvim) -> None:
     """sort_markdown_events
 
@@ -43,44 +31,3 @@ def sort_markdown_events(nvim: Nvim) -> None:
         return
 
     set_schedule_from_events_list(nvim, sorted_events, True)
-
-
-def split_line(line: str) -> List[str]:
-    """split_line
-
-    Splits a line at 79 characters, such that it can be wrapped.
-    """
-
-    if len(line) <= 79:
-        return [line]
-
-    reversed_line: str = line[:80][::-1]
-    space_index: int = reversed_line.find(" ")
-
-    before_space_index: int = 80 - space_index - 1
-    after_space_index: int = 80 - space_index
-
-    lines: List[str] = [line[:before_space_index]]
-
-    padding: int = get_line_padding(line)
-    line_remainder: str = apply_padding(line[after_space_index:], padding)
-
-    return lines + split_line(line_remainder)
-
-
-def get_line_padding(line: str) -> int:
-    """get_line_padding
-
-    Return the indentation of the line before the bullet.
-    """
-
-    return len(line) - len(line.lstrip(" "))
-
-
-def apply_padding(line: str, padding: int) -> str:
-    """apply_padding
-
-    Adds the required number of spaces to the current line.
-    """
-
-    return f"{padding * ' '}{line}"
